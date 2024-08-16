00e4ba77aca261e5fa881a09a9cfdadd461ed7c9
arifadnan05 <arifahamedadnan@gmail.com>
2024-08-16 09:18:02 +0600

    login components are added

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
     // {
@@ -8,7 +9,8 @@ const router = createBrowserRouter([
     // }
 
     {
-        
+        path: '/',
+        element: <Login />
     }
 ])
 export default router
